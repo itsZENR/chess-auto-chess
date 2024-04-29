7c124742f13cba5c61fb526b0ea44625cdd38a2e
itszenr <zenRbalahnine@gmail.com>
2024-04-29 18:58:45 +0700

    IP_reg

--- app/ChessAutoChess/views.py
+++ app/ChessAutoChess/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse(request.META['HTTP_X_REAL_IP'])
+    user = authenticate(username=request.META['HTTP_X_REAL_IP'], password='mypassword')
+    if user is not None:
+        return HttpResponse(request.META['HTTP_X_REAL_IP'])
+    else:
+        user = User.objects.create_user(request.META['HTTP_X_REAL_IP'], 'myemail@crazymail.com', 'mypassword')
+
+        # Обновите поля и сохраните их снова
+        user.first_name = 'John'
+        user.last_name = 'Citizen'
+        user.save()
+        return HttpResponse('Пользователь создан')
