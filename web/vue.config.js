98e67e6e9fed33022e31c5ae27147a2ce24c0f5d
Dmitriy Aratin <dima.aratin0@gmail.com>
2024-04-30 11:34:52 +0700

    chore: Мелкие правки и улучшения

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,30 +1,29 @@
-const {defineConfig} = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
-const {VuetifyPlugin} = require('webpack-plugin-vuetify')
+const { VuetifyPlugin } = require('webpack-plugin-vuetify');
 
 module.exports = defineConfig({
-    transpileDependencies: ["vuetify"],
-    publicPath: process.env.VUE_APP_NODE_ENV !== "development" ? "/static" : "/",
-    devServer: {
-        port: 8080
-    },
-    configureWebpack: {
-        plugins: [
-            new webpack.DefinePlugin({
-                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-            })
-        ],
-    },
-    pluginOptions: {
-        vuetify: {
-            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-        },
-        plugins: [
-            new VuetifyPlugin(
-                {
-                    autoImport: true,
-                    styles: {configFile: 'src/css/main.module.css'}
-                }),
-        ],
-    },
-})
+	transpileDependencies: ['vuetify'],
+	publicPath: process.env.VUE_APP_NODE_ENV === 'development' ? '/static' : '/',
+	devServer: {
+		port: 8080,
+	},
+	configureWebpack: {
+		plugins: [
+			new webpack.DefinePlugin({
+				__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+			}),
+		],
+	},
+	pluginOptions: {
+		vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		},
+		plugins: [
+			new VuetifyPlugin({
+				autoImport: true,
+				styles: { configFile: 'src/css/main.module.css' },
+			}),
+		],
+	},
+});
