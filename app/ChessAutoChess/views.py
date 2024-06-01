799714986e9744fdee692cf0abd23cc146a98559
itszenr <zenRbalahnine@gmail.com>
2024-06-01 09:35:03 +0700

    all

--- app/ChessAutoChess/views.py
+++ app/ChessAutoChess/views.py
@@ -6,14 +6,24 @@ from django.contrib.auth import authenticate
 
 
 def index(request):
-    user = authenticate(username=request.META['HTTP_X_REAL_IP'], password='mypassword')
+    '''
+    Аворизация и регистрация пользователя по IP
+    '''
+    USERNAME = request.META['REMOTE_ADDR']
+    user = authenticate(username=USERNAME, password='mypassword')
     if user is not None:
-        return HttpResponse(request.META['HTTP_X_REAL_IP'])
+        return HttpResponse(USERNAME)
     else:
-        user = User.objects.create_user(request.META['HTTP_X_REAL_IP'], 'myemail@crazymail.com', 'mypassword')
+        user = User.objects.create_user(USERNAME, 'myemail@crazymail.com', 'mypassword')
 
         # Обновите поля и сохраните их снова
         user.first_name = 'John'
         user.last_name = 'Citizen'
         user.save()
         return HttpResponse('Пользователь создан')
+
+@login_required
+def index(request):
+    game = Game(white_player=request.user, black_player=request.user)
+    game.save()
+    return render(request, "game/game.html", {"room_name": game.pk})
