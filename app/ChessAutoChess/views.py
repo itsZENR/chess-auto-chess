fafa4a14878593958c29adb6b1715bde1442d377
itszenr <zenRbalahnine@gmail.com>
2024-06-02 09:33:36 +0700

    fix: rename USERNAME to username for snake_case

--- app/ChessAutoChess/views.py
+++ app/ChessAutoChess/views.py
@@ -1,24 +1,28 @@
+'''
+Логика endpoint'ов на сранице игры
+'''
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
-from ChessAutoChess.serializers import UserSerializer
 from rest_framework.viewsets import ModelViewSet
+from ChessAutoChess.serializers import UserSerializer
+
 
 
 def index(request):
     '''
     Аворизация и регистрация пользователя по IP
     '''
-    USERNAME = request.META['REMOTE_ADDR']
-    user = authenticate(username=USERNAME, password='mypassword')
+    username = request.META['REMOTE_ADDR']
+    user = authenticate(username=username, password='mypassword')
     if user is not None:
         # return HttpResponse(USERNAME)
         # return render(request, "game.html", {"room_name": game.pk})
         return render(request, "game.html")
     else:
-        user = User.objects.create_user(USERNAME, 'myemail@crazymail.com', 'mypassword')
+        user = User.objects.create_user(username, 'myemail@crazymail.com', 'mypassword')
 
         # Обновите поля и сохраните их снова
         user.first_name = 'John'
