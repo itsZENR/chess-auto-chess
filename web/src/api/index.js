7a3043671dd8065bd5bfb8657295d433bc740e73
Meng H00 <zhmuruk1@gmail.com>
2024-06-03 09:15:32 +0300

    add axios

---
+++ web/src/api/index.js
@@ -0,0 +1,26 @@
+import axios  from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://127.0.0.1',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const getUsers = async () => {
+    try {
+        await apiClient.get('api/users');
+    } catch (error) {
+        console.error('Error get users:', error);
+        throw error;
+    }
+};
+
+// export const sendUserData = async (userData) => {
+//     try {
+//         await apiClient.post('api/users', userData);
+//     } catch (error) {
+//         console.error('Error sending user data:', error);
+//         throw error;
+//     }
+// };
