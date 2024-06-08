f88a8d1b98f746e9ef202dee73b74e6b46333b9f
Meng H00 <zhmuruk1@gmail.com>
2024-06-08 08:50:20 +0300

    add auth

--- web/src/api/authCookie.js
+++ web/src/api/authCookie.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 export function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -14,17 +12,3 @@ export function getCookie(name) {
     }
     return cookieValue;
 }
-
-const api = axios.create({
-  baseURL: 'https://127.0.0.1',
-});
-
-api.interceptors.request.use(config => {
-  const csrfToken = document.cookie.split('; ').find(row => row.startsWith('csrfToken='));
-  if (csrfToken) {
-    config.headers['X-CSRF-Token'] = csrfToken.split('=')[1];
-  }
-  return config;
-});
-
-export default api;
