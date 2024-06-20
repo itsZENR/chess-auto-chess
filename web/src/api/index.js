3a2f778490da71bcbeb3db679caac4b2ee6f8631
Meng H00 <zhmuruk1@gmail.com>
2024-06-20 13:42:11 +0300

    delete url

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import {getCookie, getAccessToken, setAccessToken} from "@/api/authCookie";
 
-const url = process.env.VUE_APP_URL
 const api = axios.create({
-    baseURL: `http://${url}`,
     headers: {
         'Content-Type': 'application/json',
     },
