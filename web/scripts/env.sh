7c72f1fce2e7bb5058e52f86c0e47ebf5d085085
Meng H00 <zhmuruk1@gmail.com>
2024-06-25 06:17:47 +0300

    add link for social share

--- web/scripts/env.sh
+++ web/scripts/env.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 rm -rf /var/www/chessautochess5/env-config.js
 touch /var/www/chessautochess5/env-config.js
-
-
 if [[ ! -z $NGINX_USE_HTTPS ]]; then
   echo "window.__use_https = \"$NGINX_USE_HTTPS\";" >> /var/www/chessautochess5/env-config.js
 fi
